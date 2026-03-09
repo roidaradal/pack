8716bdc59cf949c1108b3546b29d1240c528493d
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-09 11:16:03 +0800

    Boolean conv functions

---
+++ conv/conv.go
@@ -0,0 +1,25 @@
+// Package conv contains basic data type conversion functions
+package conv
+
+import "fmt"
+
+// BoolToInt converts bool to int (true = 1, false = 0)
+func BoolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+// BoolToUint converts bool to uint (true = 1, false = 0)
+func BoolToUint(b bool) uint {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+// BoolToString converts bool to string
+func BoolToString(b bool) string {
+	return fmt.Sprintf("%t", b)
+}
