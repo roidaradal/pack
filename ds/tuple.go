782df9490314c74ecb0167557c945ce11d2f5288
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-10 08:45:11 +0800

    Tuple3 type

--- ds/tuple.go
+++ ds/tuple.go
@@ -6,12 +6,29 @@ type Tuple2[A, B any] struct {
 	V2 B
 }
 
+// Tuple3 is a tuple with three types
+type Tuple3[A, B, C any] struct {
+	V1 A
+	V2 B
+	V3 C
+}
+
 // NewTuple2 creates a new Tuple2
 func NewTuple2[A, B any](v1 A, v2 B) Tuple2[A, B] {
 	return Tuple2[A, B]{v1, v2}
 }
 
+// NewTuple3 creates a new Tuple3
+func NewTuple3[A, B, C any](v1 A, v2 B, v3 C) Tuple3[A, B, C] {
+	return Tuple3[A, B, C]{v1, v2, v3}
+}
+
 // Values returns the unpacked Tuple2 values
 func (t Tuple2[A, B]) Values() (A, B) {
 	return t.V1, t.V2
+}
+
+// Values returns the unpacked Tuple3 values
+func (t Tuple3[A, B, C]) Values() (A, B, C) {
+	return t.V1, t.V2, t.V3
 }
--- ds/tuple_test.go
+++ ds/tuple_test.go
@@ -4,12 +4,24 @@ import "testing"
 
 func TestTuple2(t *testing.T) {
 	v1, v2 := "apple", 5
-	t1 := NewTuple2(v1, v2)
-	if t1.V1 != v1 || t1.V2 != v2 {
-		t.Errorf("Tuple2.V1, V2 = %v, %v, want %v, %v", t1.V1, t1.V2, v1, v2)
+	t2 := NewTuple2(v1, v2)
+	if t2.V1 != v1 || t2.V2 != v2 {
+		t.Errorf("Tuple2.V1, V2 = %v, %v, want %v, %v", t2.V1, t2.V2, v1, v2)
 	}
-	a, b := t1.Values()
+	a, b := t2.Values()
 	if a != v1 || b != v2 {
 		t.Errorf("Tuple2.Values() = %v, %v, want %v, %v", a, b, v1, v2)
+	}
+}
+
+func TestTuple3(t *testing.T) {
+	v1, v2, v3 := "apple", 5, 12.5
+	t3 := NewTuple3(v1, v2, v3)
+	if t3.V1 != v1 || t3.V2 != v2 || t3.V3 != v3 {
+		t.Errorf("Tuple3.V1, V2, V3 = %v, %v, %v want %v, %v, %v", t3.V1, t3.V2, t3.V3, v1, v2, v3)
+	}
+	a, b, c := t3.Values()
+	if a != v1 || b != v2 || c != v3 {
+		t.Errorf("Tuple3.Values() = %v, %v, %v want %v, %v, %v", a, b, c, v1, v2, v3)
 	}
 }
