18f03cb68457e3c7da3c72f658a77aef80a0787a
roidaradal <roidaradal@gmail.com>
2026-03-12 21:42:31 +0800

    Test List random methods

--- ds/list_test.go
+++ ds/list_test.go
@@ -51,18 +51,51 @@ func TestListMethods(t *testing.T) {
 	// TODO: RemoveFunc, RemoveAllFunc
 	// TODO: Get, GetFuncOrDefault
 	// TODO: Last, MustLast
-	// TODO: GetRandom, MustGetRandom
-	// TODO: Shuffle
 }
 
-func TestCheckMethods(t *testing.T) {
+func TestListRandom(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("List.MustGetRandom() did not panic")
+		}
+	}()
+	l1 := NewList[int](3) // empty
+	for range 5 {
+		item := l1.GetRandom()
+		if !item.IsNil() {
+			t.Errorf("EmptyList.GetRandom() = %v, want nil", item)
+		}
+	}
+	l := NewInclusiveRange(1, 100).ToList()
+	for range 100 {
+		item := l.GetRandom()
+		value := item.Value()
+		if item.IsNil() || !(1 <= value && value <= 100) {
+			t.Errorf("List.GetRandom() = %v, want 1..100", item)
+		}
+		value = l.MustGetRandom()
+		if !(1 <= value && value <= 100) {
+			t.Errorf("List.MustGetRandom() = %v, want 1..100", item)
+		}
+	}
+	l2 := List[int]{1, 2, 3, 4, 5, 6, 7}
+	l3 := l2.Copy()
+	l3.Shuffle()
+	if slices.Equal(l2, l3) == true {
+		t.Errorf("List.Shuffle = %v, want not original %v", l3, l2)
+	}
+
+	l1.MustGetRandom() // should panic (empty list)
+}
+
+func TestListCheck(t *testing.T) {
 	// TODO: Any, AnyIndexed
 	// TODO: NotAny, NotAnyIndexed
 	// TODO: All, AllIndexed
 	// TODO: CountFunc
 }
 
-func TestFnMethods(t *testing.T) {
+func TestListFn(t *testing.T) {
 	// TODO: MapList
 	// TODO: Filter, FilterIndexed
 	// TODO: Reduce
