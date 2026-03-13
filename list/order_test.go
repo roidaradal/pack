b26183384fd17075f81f45a540f9c625a78e171a
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-13 09:39:25 +0800

    Test list order functions

--- list/order_test.go
+++ list/order_test.go
@@ -3,8 +3,27 @@ package list
 import "testing"
 
 func TestOrderFunctions(t *testing.T) {
-	// TODO: AllGreater, AllGreaterEqual
-	// TODO: AllLesser, AllLesserEqual
+	type testCase struct {
+		name   string
+		want   bool
+		actual bool
+	}
+	items := InclusiveRange(1, 10)
+	testCases := []testCase{
+		{"AllGreater", true, AllGreater(items, 0)},
+		{"AllGreater", false, AllGreater(items, 5)},
+		{"AllGreaterEqual", true, AllGreaterEqual(items, 1)},
+		{"AllGreaterEqual", false, AllGreaterEqual(items, 20)},
+		{"AllLesser", true, AllLesser(items, 20)},
+		{"AllLesser", false, AllLesser(items, 10)},
+		{"AllLesserEqual", true, AllLesserEqual(items, 10)},
+		{"AllLesserEqual", false, AllLesserEqual(items, 7)},
+	}
+	for _, x := range testCases {
+		if x.want != x.actual {
+			t.Errorf("%s: want %t, got %t", x.name, x.want, x.actual)
+		}
+	}
 }
 
 func TestArgMinMax(t *testing.T) {
