48b1dd4cf89934428d2711e94aac8c739447e4d0
roidaradal <roidaradal@gmail.com>
2026-03-17 08:54:14 +0800

    QB combos tests

--- qb/combo_test.go
+++ qb/combo_test.go
@@ -1,13 +1,74 @@
 package qb
 
-import "testing"
+import (
+	"slices"
+	"testing"
+
+	"github.com/roidaradal/pack/list"
+)
 
 func TestCombos(t *testing.T) {
-	// TODO: NoConditionTest
-	// TODO: EqualTest, NotEqualTest
-	// TODO: PrefixTest, SuffixTest, SubstringTest
-	// TODO: GreaterTest, GreaterEqualTest
-	// TODO: LesserTest, LesserEqualTest
-	// TODO: InTest, NotInTest
-	// TODO: AndTest, OrTest
+	type Person struct {
+		Name    string
+		Address string
+		Age     int
+		Job     string
+		Score   int
+	}
+	type testCase struct {
+		cond      DualCondition[Person]
+		wantBools []bool
+	}
+	this := NewInstance(MySQL)
+	p := &Person{}
+	err := AddType(this, p)
+	if err != nil {
+		t.Errorf("AddType error: %v", err)
+	}
+
+	persons := []Person{
+		{"John", "Astra Tower, Gotham City", 30, "manager", 95},
+		{"Jane", "Star Village, India", 65, "assistant ceo", 85},
+		{"Jill", "Tower of Doom, Japan", 20, "dev", 55},
+		{"Jim", "Sub Division, Singapore City", 40, "qa", 67},
+		{"Jack", "N/A", 18, "intern", 50},
+	}
+
+	condNone := NoConditionTest[Person]()
+	condEqual := EqualTest[Person](this, &p.Name, "John")
+	condNotEqual := NotEqualTest[Person](this, &p.Job, "manager")
+	condPrefix := PrefixTest[Person](this, &p.Job, "assistant")
+	condSuffix := SuffixTest[Person](this, &p.Address, "City")
+	condSubstring := SubstringTest[Person](this, &p.Address, "Tower")
+	condGreater := GreaterTest[Person](this, &p.Score, 75)
+	condGreaterEqual := GreaterEqualTest[Person](this, &p.Age, 20)
+	condLesser := LesserTest[Person](this, &p.Age, 60)
+	condLesserEqual := LesserEqualTest[Person](this, &p.Score, 50)
+	condIn := InTest[Person](this, &p.Job, []string{"dev", "qa", "intern"})
+	condNotIn := NotInTest[Person](this, &p.Score, []int{67, 69})
+	condAnd := AndTest[Person](condEqual, condGreater)
+	condOr := OrTest[Person](condLesserEqual, condIn)
+
+	testCases := []testCase{
+		{condNone, []bool{true, true, true, true, true}},
+		{condEqual, []bool{true, false, false, false, false}},
+		{condNotEqual, []bool{false, true, true, true, true}},
+		{condPrefix, []bool{false, true, false, false, false}},
+		{condSuffix, []bool{true, false, false, true, false}},
+		{condSubstring, []bool{true, false, true, false, false}},
+		{condGreater, []bool{true, true, false, false, false}},
+		{condGreaterEqual, []bool{true, true, true, true, false}},
+		{condLesser, []bool{true, false, true, true, true}},
+		{condLesserEqual, []bool{false, false, false, false, true}},
+		{condIn, []bool{false, false, true, true, true}},
+		{condNotIn, []bool{true, true, true, false, true}},
+		{condAnd, []bool{true, false, false, false, false}},
+		{condOr, []bool{false, false, true, true, true}},
+	}
+	for _, x := range testCases {
+		actualBools := list.Map(persons, x.cond.Test)
+		if slices.Equal(actualBools, x.wantBools) == false {
+			t.Errorf("Combo.Test() = %v, want %v", actualBools, x.wantBools)
+		}
+	}
 }
